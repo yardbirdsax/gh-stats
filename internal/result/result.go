09791b20e0f8bd11bcacbef70b616b0a5e531edc
Josh Feierman <josh@sqljosh.com>
2022-11-13 19:05:54 -0500

    wip result

--- internal/result/result.go
+++ internal/result/result.go
@@ -2,37 +2,52 @@
 package result
 
 import (
-  "github.com/jedib0t/go-pretty/v6/table"
+	"fmt"
+
+	"github.com/jedib0t/go-pretty/v6/table"
 )
 
-// This is a set of results that can be represented as rows of data.
-type tabularResult [][]interface{}
+type Results [][]interface{}
 
-type Result struct {
-  tabularResults tabularResult
+func FromSlice(columnNames []interface{}, data [][]interface{}) (*Results, error) {
+	result := Results{}
+
+  // Write the header row
+  columnCount := len(columnNames)
+  result = append(result, columnNames)
+
+  // Write the rest of the data
+  for _, d := range data {
+    if len(d) != columnCount {
+      return &Results{}, fmt.Errorf("one or more rows contain a different number of columns than what are named")
+    }
+  }
+  result = append(result, data...)
+
+	return &result, nil
 }
 
 // AsMarkdownTable renders tabular results as a Markdown table.
-func (r *Result) AsMarkdownTable() string {
-  var renderedString string
-  t := table.NewWriter()
+func (r *Results) AsMarkdownTable() string {
+	var renderedString string
+	t := table.NewWriter()
 
-  // Construct the table header
-  firstRow := r.tabularResults[0]
-  tableHeaderRow := table.Row{}
-  for _, k := range firstRow {
-    tableHeaderRow = append(tableHeaderRow, k)
-  }
-  t.AppendHeader(tableHeaderRow)
+	// Construct the table header
+	firstRow := (*r)[0]
+	tableHeaderRow := table.Row{}
+	for _, k := range firstRow {
+		tableHeaderRow = append(tableHeaderRow, k)
+	}
+	t.AppendHeader(tableHeaderRow)
 
-  for _, result := range r.tabularResults[1:] {
-    tableRow := table.Row{}
-    for _, colVal := range result {
-      tableRow = append(tableRow, colVal)
-    }
-    t.AppendRow(tableRow)
-  }
+	for _, result := range (*r)[1:] {
+		tableRow := table.Row{}
+		for _, colVal := range result {
+			tableRow = append(tableRow, colVal)
+		}
+		t.AppendRow(tableRow)
+	}
 
-  renderedString = t.RenderMarkdown()
-  return renderedString
+	renderedString = t.RenderMarkdown()
+	return renderedString
 }
