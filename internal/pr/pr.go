b29da1ff7ddc865b44e16199241e05cbe9154886
Josh Feierman <josh@sqljosh.com>
2022-11-22 11:08:36 -0500

    wip team reviews

--- internal/pr/pr.go
+++ internal/pr/pr.go
@@ -1,50 +1,87 @@
 package pr
 
 import (
+	"fmt"
+  //"net/url"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/cli/go-gh"
+	"github.com/cli/go-gh/pkg/api"
+	"github.com/cli/go-gh/pkg/auth"
 	"github.com/yardbirdsax/gh-stats/internal/result"
 )
 
 func MyReviews() (*result.Results, error) {
-	var myReviews *result.Results
+	filter := "is:pr reviewed-by:@me"
+  return getIssueCount(filter)
+}
 
-	client, err := gh.RESTClient(nil)
+func TeamReviews(orgName string, teamName string) (*result.Results, error) {
+	client, err := getClient()
 	if err != nil {
-		return myReviews, err
+		return nil, err
+	}
+
+	teamResponse := &teamMemberResponse{}
+	err = client.Get(fmt.Sprintf("/orgs/%s/teams/%s/members", orgName, teamName), teamResponse)
+	if err != nil {
+		return nil, err
+	}
+	teamMembers := []string{}
+	for _, member := range *teamResponse {
+		teamMembers = append(teamMembers, fmt.Sprintf("reviewed-by:%s", member.Login))
+	}
+
+	filter := fmt.Sprintf("is:pr (%s) user:%s", strings.Join(teamMembers, " "), orgName)
+	return getIssueCount(filter)
+}
+
+func getClient() (api.RESTClient, error) {
+	token, _ := auth.TokenForHost("github.com")
+	return gh.RESTClient(&api.ClientOptions{
+		AuthToken: token,
+	})
+}
+
+func getIssueCount(filter string) (*result.Results, error) {
+	var results *result.Results
+
+	client, err := getClient()
+	if err != nil {
+		return results, err
 	}
 
 	response := issueSearchResponse{}
-	err = client.Get("search/issues?q=is:pr+reviewed-by:@me", &response)
+	err = client.Get(fmt.Sprintf("search/issues?q=%s", filter), &response)
 	if err != nil {
-		return myReviews, err
+		return results, err
 	}
 
-  columnNames := []interface{}{
-    "created date",
-    "count",
-  }
+	columnNames := []interface{}{
+		"created date",
+		"count",
+	}
 
-  mapData := make(map[time.Time]int)
+	mapData := make(map[time.Time]int)
 	for _, i := range response.Items {
 		roundedDate := i.CreatedAt.Truncate(24 * time.Hour)
-    mapData[roundedDate] += 1
+		mapData[roundedDate] += 1
 	}
 
-  data := make([][]interface{}, 0, len(mapData))
-  for k, v := range mapData {
-    data = append(data, []interface{}{k, v})
-  }
-  sort.Slice(data, func(i, j int) bool {
-    firstValue := data[i][0].(time.Time)
-    secondValue := data[j][0].(time.Time)
-    return firstValue.Before(secondValue)
-  })
+	data := make([][]interface{}, 0, len(mapData))
+	for k, v := range mapData {
+		data = append(data, []interface{}{k, v})
+	}
+	sort.Slice(data, func(i, j int) bool {
+		firstValue := data[i][0].(time.Time)
+		secondValue := data[j][0].(time.Time)
+		return firstValue.Before(secondValue)
+	})
 
-	myReviews, err = result.FromSlice(columnNames, data)
-  return myReviews, err
+	results, err = result.FromSlice(columnNames, data)
+	return results, err
 }
 
 type issueSearchResponse struct {
@@ -55,4 +92,10 @@ type issueSearchResponse struct {
 
 type issueSearchItem struct {
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type teamMemberResponse []teamMember
+
+type teamMember struct {
+	Login string `json:"login"`
 }
--- internal/pr/pr_test.go
+++ internal/pr/pr_test.go
@@ -1,6 +1,7 @@
 package pr
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -34,6 +35,49 @@ func TestMyReviews(t *testing.T) {
   }
 
   actualResults, err := MyReviews()
+
+  require.NoError(t, err)
+  assert.Equal(t, expectedResults, actualResults)
+  assert.False(t, gock.IsPending(), "gock has pending requests")
+  assert.False(t, gock.HasUnmatchedRequest(), "gock has unmatched requests")
+}
+
+func TestTeamReviews(t *testing.T) {
+  defer gock.Off()
+  gock.Observe(gock.DumpRequest)
+  expectedOrgName := "org"
+  expectedTeamname := "team"
+  gock.New("https://api.github.com").Get(fmt.Sprintf("/orgs/%s/teams/%s/members", expectedOrgName, expectedTeamname)).Reply(200).JSON(
+    []map[string]interface{}{
+      {
+        "login": "user1",
+      },
+      {
+        "login": "user2",
+      },
+    },
+  )
+  gock.New("https://api.github.com").Get("search/issues").MatchParam("q", "is:pr \\(reviewed-by:user1 reviewed-by:user2\\) user:org").Reply(200).JSON(
+    map[string]interface{}{
+      "total_count": 10,
+      "incomplete_results": false,
+      "items": []map[string]interface{}{
+        {
+          "created_at": "2022-11-02T20:20:20Z",
+        },
+        {
+          "created_at": "2022-11-01T20:20:20Z",
+        },
+      },
+    },
+  )
+  expectedResults := &result.Results{
+    {"created date", "count"},
+    {time.Date(2022,11,1, 0, 0, 0, 0, time.UTC),  1},
+    {time.Date(2022,11,2, 0, 0, 0, 0, time.UTC), 1},
+  }
+
+  actualResults, err := TeamReviews(expectedOrgName, expectedTeamname)
 
   require.NoError(t, err)
   assert.Equal(t, expectedResults, actualResults)
